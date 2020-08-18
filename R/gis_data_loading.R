637d090a3db618562b50e5fd9e6c579f211082b8
hrvg <herve@guillon.xyz>
2020-08-18 16:59:52 -0700

    Changed variable name hydro_class to region

--- R/gis_data_loading.R
+++ R/gis_data_loading.R
@@ -1,54 +1,53 @@
 #' Load a SpatialLinesDataFrame containing values for valley confinement, GIS slope and RUSLE.
 #' The SpatialDataFrame source data is the NHDv2Plus dataset which is segmented into 200-m stream intervals.
-#' @param hydro_class
-#' @param stream_order logical, if 
+#' @param region
+#' @param stream_order logical, if `TRUE` changing the file loaded for a given region
 #' @return a SpatialLinesDataFrame
-#' @import sf
-#' @import sp
-get_conf_gis <- function(hydro_class, stream_order = FALSE){
+#' @export
+get_conf_gis <- function(region, stream_order = FALSE){
 	conf_gis_dir <- "extdata/gis-files/200m_streams/"
-	if (hydro_class == "SFE"){
+	if (region == "SFE"){
 		conf_gis_dir <- "/data/california-rivers/gis-files/SFE_200m_1standhigher_order"
 		conf_gis_shp <- "SFE_1standhigher_binned200m_streams_VAA_LDD.shp"
-		conf_gis <- read_sf(file.path(conf_gis_dir,conf_gis_shp))
+		conf_gis <- sf::read_sf(file.path(conf_gis_dir,conf_gis_shp))
 		conf_gis <- as(conf_gis, "Spatial")
-	} else if (hydro_class == "NC"){
+	} else if (region == "NC"){
 		conf_gis_shp <- "North_200m_with_first_order_VAA_LDD.shp"
-		conf_gis <- read_sf(file.path(conf_gis_dir,conf_gis_shp))
+		conf_gis <- sf::read_sf(file.path(conf_gis_dir,conf_gis_shp))
 		conf_gis <- as(conf_gis, "Spatial")
-	} else if (hydro_class == "NCC"){
+	} else if (region == "NCC"){
 		conf_gis_shp <- "NorthCentral_200m_with_first_order_VAA_LDD.shp"
-		conf_gis <- read_sf(file.path(conf_gis_dir,conf_gis_shp))
+		conf_gis <- sf::read_sf(file.path(conf_gis_dir,conf_gis_shp))
 		conf_gis <- as(conf_gis, "Spatial")
-	} else if (hydro_class == "SCC"){
+	} else if (region == "SCC"){
 		conf_gis_shp <- "SouthCentral_200m_with_first_order_VAA_LDD.shp"
-		conf_gis <- read_sf(file.path(conf_gis_dir,conf_gis_shp))
+		conf_gis <- sf::read_sf(file.path(conf_gis_dir,conf_gis_shp))
 		conf_gis <- as(conf_gis, "Spatial")
-	} else if (hydro_class == "SC"){
+	} else if (region == "SC"){
 		conf_gis_shp <- "South_200m_VAA_LDD.shp"
-		conf_gis <- read_sf(file.path(conf_gis_dir,conf_gis_shp))			
+		conf_gis <- sf::read_sf(file.path(conf_gis_dir,conf_gis_shp))			
 		conf_gis <- as(conf_gis, "Spatial")
-	} else if (hydro_class == "SECA"){
+	} else if (region == "SECA"){
 		conf_gis_shp <- "SouthEastCalifornia_200m_VAA_LDD.shp"
-		conf_gis <- read_sf(file.path(conf_gis_dir,conf_gis_shp))			
+		conf_gis <- sf::read_sf(file.path(conf_gis_dir,conf_gis_shp))			
 		conf_gis <- as(conf_gis, "Spatial")	
 		attr_names <- names(conf_gis)
 		attr_names[which(attr_names == "Confinemen")] <- "CONFINEMEN"
 		attr_names[which(attr_names == "Slope")] <- "SLOPE"
 		attr_names[which(attr_names == "Area")] <- "AREA"
 		names(conf_gis) <- attr_names
-	} else if (hydro_class == "SJT"){
+	} else if (region == "SJT"){
 		conf_gis_shp <- "SanJoaquin_200m_VAA_LDD.shp"
-		conf_gis <- read_sf(file.path(conf_gis_dir,conf_gis_shp))			
+		conf_gis <- sf::read_sf(file.path(conf_gis_dir,conf_gis_shp))			
 		conf_gis <- as(conf_gis, "Spatial")
 		attr_names <- names(conf_gis)
 		attr_names[which(attr_names == "Confinemen")] <- "CONFINEMEN"
 		attr_names[which(attr_names == "Slope")] <- "SLOPE"
 		attr_names[which(attr_names == "Area")] <- "AREA"
 		names(conf_gis) <- attr_names
-	} else if (hydro_class == "K"){
+	} else if (region == "K"){
 		conf_gis_shp <- "Klamath_200m_VAA_LDD.shp"
-		conf_gis <- read_sf(file.path(conf_gis_dir,conf_gis_shp))			
+		conf_gis <- sf::read_sf(file.path(conf_gis_dir,conf_gis_shp))			
 		conf_gis <- as(conf_gis, "Spatial")
 		attr_names <- names(conf_gis)
 		attr_names[which(attr_names == "Confinemen")] <- "CONFINEMEN"
@@ -61,7 +60,7 @@ get_conf_gis <- function(hydro_class, stream_order = FALSE){
 		} else {
 			conf_gis_shp <- "Sacramento_200mbin_variables.shp"
 		}
-		conf_gis <- read_sf(file.path(conf_gis_dir,conf_gis_shp))			
+		conf_gis <- sf::read_sf(file.path(conf_gis_dir,conf_gis_shp))			
 		conf_gis <- as(conf_gis, "Spatial")
 	}
 	conf_gis$ID <- seq(nrow(conf_gis))
